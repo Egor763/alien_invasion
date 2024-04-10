0401bf3c8fbc2223ebd70d4a02164675e363fd81
Egor763 <132680057+Egor763@users.noreply.github.com>
2024-04-10 15:02:02 +0400

    движение

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,15 +18,32 @@ class AlienInvasion:
     def run_game(self):
         # запуск основного цикла игры
         while True:
-            # отслеживание событий клавиатуры и мыши
+            self.ship.update()
+            # ослеживание событий клавиатуры и мыши
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
 
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+
+                    elif event.key == pygame.K_q:
+                        exit()
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
-
             # отображение последнего прорисованного экрана
             pygame.display.flip()
 
