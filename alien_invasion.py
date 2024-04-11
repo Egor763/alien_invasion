0e6e1ebe0b39f607d7334826ca9303b233ec9cb5
Egor763 <132680057+Egor763@users.noreply.github.com>
2024-04-11 15:36:18 +0400

    добавление пришельцев

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+from alien import Alien
 
 
 # Класс для управлениями ресурсами и поведением игры
@@ -14,11 +16,23 @@ class AlienInvasion:
         pygame.display.set_caption("Инопланетное вторжение")
 
         self.ship = Ship(self)
+        self.alien = Alien(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self.aliens.add(self.alien)
 
     def run_game(self):
         # запуск основного цикла игры
+
         while True:
             self.ship.update()
+
+            self.bullets.update()
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+
             # ослеживание событий клавиатуры и мыши
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -35,6 +49,9 @@ class AlienInvasion:
                     elif event.key == pygame.K_q:
                         exit()
 
+                    elif event.key == pygame.K_SPACE:
+                        fire_bullet(self)
+
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_RIGHT:
                         self.ship.moving_right = False
@@ -42,8 +59,34 @@ class AlienInvasion:
                     elif event.key == pygame.K_LEFT:
                         self.ship.moving_left = False
 
+            def create_fleet(self):
+                alien = Alien(self)
+                alien_width = alien.rect.width
+                avariable_space_x = self.settings.W - (2 * alien_width)
+                number_aliens_x = avariable_space_x // (2 * alien_width)
+
+                for alien_number in range(number_aliens_x):
+                    alien = Alien(self)
+                    alien.x = alien_width + 2 * alien_width * alien_number
+                    alien.rect.x = alien.x
+                    self.aliens.add(alien)
+
+            create_fleet(self)
+
+            def fire_bullet(self):
+                if len(self.bullets) < self.settings.bullets_allowed:
+                    new_bullet = Bullet(self)
+                    self.bullets.add(new_bullet)
+
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
+
+            for bullet in self.bullets.sprites():
+                bullet.draw_bullet()
+            self.aliens.draw(self.screen)
+
+            pygame.display.flip()
+
             # отображение последнего прорисованного экрана
             pygame.display.flip()
 
--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship:
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load("spaceship.png")
+        self.image = pygame.image.load("images/spaceship.png")
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.screen_rect.midbottom
 
